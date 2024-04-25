ca3fd8ef178eaa3cea149e4ee6cbf51357291949
elliscode <45629640+elliscode@users.noreply.github.com>
2024-04-24 21:45:56 -0400

    release script added deviled eggs

---
+++ release.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+lambda=false;
+s3=false;
+for flag in "$@"
+do
+    case "${flag}" in
+        "l") lambda=true;;
+        "s") s3=true;;
+        "ls") s3=true;lambda=true;;
+        "sl") s3=true;lambda=true;;
+    esac
+done
+if $lambda; then
+    echo "Lambda selected";
+fi
+if $s3; then
+    echo "S3 selected";
+fi
+if ! $lambda && ! $s3; then
+    echo "You can supply either l or s, or ls, you supplied nothing, so I will do nothing";
+    echo "";
+    echo "./${ENV}-release.sh ls";
+fi
+
+if $lambda; then
+    cd lambda/
+    TIMESTAMP=$(date +%s)
+    zip -vr ../lambda-release-${TIMESTAMP}.zip . -x "*.DS_Store"
+    cd ../
+    aws lambda update-function-code --function-name=ellisrecipes-generate --zip-file=fileb://lambda-release-${TIMESTAMP}.zip --no-cli-pager
+fi
+
+if $s3; then
+    aws s3 sync s3 s3://daniel-townsend-ellisrecipes --exclude "*env.js" --exclude "*.DS_Store" --delete
+fi
---
+++ s3/markdown/Deviled Eggs.md
@@ -0,0 +1,26 @@
+# Deviled Eggs
+
+## Ingredients
+
+- `6` large eggs
+- `3` tablespoons mayonnaise
+- `1` teaspoon Dijon mustard
+- `1` teaspoon apple cider vinegar
+- `1/4` tsp salt
+- paprika, for garnish
+
+## Directions
+
+- hard-boil the eggs on high heat for 12 minutes
+- peel the eggs and slice them in half vertically
+- place the egg whites on a plate, and the egg yolks in a bowl
+- combine the egg yolks with the mayonnaise, mustard, apple cider vinegar, and salt
+- place mixture in the piping bag and pipe the filling into each egg
+
+Link: https://downshiftology.com/recipes/deviled-eggs/
+
+Category: Sides
+
+Servings: 6
+
+Tags: Vegetarian, Party
